1dbef9b8f8176b2d5f4664f663a0d09a1eaef304
Ryan <ryansanders89@gmail.com>
2018-02-10 09:13:22 -0500

    There was an error in blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})http://127.0.0.1:8000/
+    return render(request, 'blog/post_list.html', {'posts': posts})
